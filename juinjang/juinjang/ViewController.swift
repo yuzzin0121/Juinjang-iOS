74e95fcd6f937bec9d03026429ec6102bcba8247
suzinlim <122861956+suzinlim@users.noreply.github.com>
2023-12-29 00:29:31 +0900

    [Build] 프로젝트 세팅

---
+++ juinjang/juinjang/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  juinjang
+//
+//  Created by 임수진 on 2023/12/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
