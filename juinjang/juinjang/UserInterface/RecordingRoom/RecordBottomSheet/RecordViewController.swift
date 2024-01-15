6d21b73b5f76fd259d50261e7473cce2053aa89d
suzinlim <122861956+suzinlim@users.noreply.github.com>
2024-01-15 16:41:36 +0900

    [Fix] bottom sheet 내 ViewController 분리

---
+++ juinjang/juinjang/UserInterface/RecordingRoom/RecordBottomSheet/RecordViewController.swift
@@ -0,0 +1,168 @@
+//
+//  RecordViewController.swift
+//  juinjang
+//
+//  Created by 임수진 on 2024/01/13.
+//
+
+import UIKit
+import SnapKit
+
+class RecordViewController: UIViewController {
+    
+    weak var bottomSheetViewController: BottomSheetViewController?
+    
+    lazy var bottomSheetView = UIView().then {
+        $0.backgroundColor = .white
+        $0.layer.cornerRadius = 30
+        $0.layer.maskedCorners = [.layerMinXMinYCorner, .layerMaxXMinYCorner]
+        $0.clipsToBounds = true
+    }
+    
+    let totalHeight: CGFloat = 844 // 전체 높이
+    let ratio: CGFloat = 392 // Bottom Sheet가 차지하는 높이
+    
+    var bottomSheetHeight: CGFloat {
+        return (ratio / totalHeight) * UIScreen.main.bounds.height // 디바이스가 달라져도 비율만큼 차지
+    }
+    
+    lazy var cancelButton = UIButton().then {
+        $0.setBackgroundImage(UIImage(named: "cancel-white"), for: .normal)
+        $0.layer.masksToBounds = true
+        $0.contentMode = .scaleAspectFill
+        $0.addTarget(self, action: #selector(cancelButtonTapped(_:)), for: .touchUpInside)
+    }
+    
+    lazy var recordLabel = UILabel().then {
+        $0.text = "녹음하기"
+        $0.textAlignment = .center
+        $0.textColor = UIColor(named: "textWhite")
+        $0.font = UIFont(name: "Pretendard-SemiBold", size: 18)
+    }
+    
+    lazy var timeLabel = UILabel().then {
+        $0.text = "00:18.79"
+        $0.textColor = UIColor(named: "lightGray")
+        $0.font = UIFont(name: "Pretendard-Bold", size: 24)
+    }
+    
+    lazy var remainTimeLabel = UILabel().then {
+        $0.text = "-00:03.21"
+        $0.textColor = UIColor(red: 1, green: 0.66, blue: 0.47, alpha: 1)
+        $0.font = UIFont(name: "Pretendard-Medium", size: 16)
+    }
+    
+    lazy var limitLabel = UILabel().then {
+        $0.text = "녹음은 5분까지만 가능해요"
+        $0.textAlignment = .center
+        $0.textColor = UIColor(red: 0.38, green: 0.38, blue: 0.38, alpha: 1)
+        $0.font = UIFont(name: "Pretendard-Medium", size: 16)
+    }
+    
+    lazy var trashButton = UIButton().then {
+        $0.setImage(UIImage(named: "record-trash"), for: .normal)
+        $0.imageView?.contentMode = .scaleAspectFill
+        $0.adjustsImageWhenHighlighted = false
+    }
+    
+    lazy var recordButton = UIButton().then {
+        $0.setImage(UIImage(named: "being-recorded-button"), for: .normal)
+        $0.imageView?.contentMode = .scaleAspectFill
+        $0.adjustsImageWhenHighlighted = false
+        $0.addTarget(self, action: #selector(startRecordPressed(_:)), for: .touchUpInside)
+    }
+    
+    lazy var completedButton = UIButton().then {
+        $0.setTitle("완료", for: .normal)
+        $0.setTitleColor(UIColor(named: "textWhite"), for: .normal)
+        $0.titleLabel?.font = UIFont(name: "Pretendard-Bold", size: 18)
+        $0.addTarget(self, action: #selector(completedButtonPressed(_:)), for: .touchUpInside)
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        bottomSheetView.backgroundColor = UIColor(named: "textBlack")
+        addSubViews()
+        setupLayout()
+    }
+    
+    func addSubViews() {
+        view.addSubview(bottomSheetView)
+        [cancelButton,
+         recordLabel,
+         timeLabel,
+         remainTimeLabel,
+         limitLabel,
+         trashButton,
+         recordButton,
+         completedButton].forEach { bottomSheetView.addSubview($0) }
+    }
+    
+    func setupLayout() {
+        bottomSheetView.snp.makeConstraints {
+            $0.leading.trailing.equalToSuperview()
+            $0.bottom.equalTo(view.snp.bottom)
+            $0.height.equalTo(bottomSheetHeight)
+        }
+        
+        cancelButton.snp.makeConstraints {
+            $0.height.equalTo(12)
+            $0.width.equalTo(12)
+//            $0.leading.equalTo(bottomSheetView.snp.leading).offset(354)
+            $0.trailing.equalTo(bottomSheetView.snp.trailing).offset(-24)
+            $0.top.equalTo(bottomSheetView.snp.top).offset(29)
+        }
+        
+        recordLabel.snp.makeConstraints {
+            $0.centerX.equalTo(bottomSheetView.snp.centerX)
+            $0.top.equalTo(bottomSheetView.snp.top).offset(24)
+//            $0.leading.equalTo(bottomSheetView.snp.leading).offset(164)
+        }
+        
+        timeLabel.snp.makeConstraints {
+            $0.centerX.equalTo(bottomSheetView.snp.centerX)
+            $0.top.equalTo(recordLabel.snp.bottom).offset(34)
+        }
+        
+        remainTimeLabel.snp.makeConstraints {
+            $0.centerX.equalTo(bottomSheetView.snp.centerX)
+            $0.top.equalTo(timeLabel.snp.bottom).offset(1)
+        }
+        
+        limitLabel.snp.makeConstraints {
+            $0.centerX.equalTo(bottomSheetView.snp.centerX)
+            $0.top.equalTo(recordLabel.snp.bottom).offset(117)        }
+        
+        trashButton.snp.makeConstraints {
+            $0.top.equalTo(limitLabel.snp.bottom).offset(48)
+            $0.leading.equalTo(bottomSheetView.snp.leading).offset(39)
+        }
+        
+        recordButton.snp.makeConstraints {
+            $0.leading.equalTo(trashButton.snp.trailing).offset(90)
+            $0.top.equalTo(limitLabel.snp.bottom).offset(35)
+        }
+        
+        completedButton.snp.makeConstraints {
+            $0.leading.equalTo(recordButton.snp.trailing).offset(90)
+            $0.top.equalTo(limitLabel.snp.bottom).offset(54)
+            $0.height.equalTo(24)
+        }
+    }
+    
+    @objc func cancelButtonTapped(_ sender: UIButton) {
+        bottomSheetViewController?.hideBottomSheetAndGoBack()
+    }
+    
+    @objc func startRecordPressed(_ sender: UIButton) {
+        if recordButton.isSelected {
+            recordButton.setImage(UIImage(named: "being-recorded-button"), for: .normal)
+        } else {
+            recordButton.setImage(UIImage(named: "record-button"), for: .normal)
+        }
+        recordButton.isSelected.toggle()
+    }
+    
+    @objc func completedButtonPressed(_ sender: UIButton) {
+    }
+}
