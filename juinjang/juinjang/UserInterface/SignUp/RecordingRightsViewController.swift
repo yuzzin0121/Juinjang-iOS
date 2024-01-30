dcaf2dcce0e6cd6f0307ba4a042070e9940e2077
suzinlim <122861956+suzinlim@users.noreply.github.com>
2024-01-30 21:51:18 +0900

    [Feat] 녹음 권한 화면 구성

---
+++ juinjang/juinjang/UserInterface/SignUp/RecordingRightsViewController.swift
@@ -0,0 +1,135 @@
+//
+//  RecordingRightsViewController.swift
+//  juinjang
+//
+//  Created by 임수진 on 1/30/24.
+//
+
+import UIKit
+
+class RecordingRightsViewController: UIViewController {
+    
+    lazy var guideLabel = UILabel().then {
+        $0.text = "주인장 앱에서\n필요한 권한을 알려드려요"
+        $0.textAlignment = .left
+        $0.numberOfLines = 2
+        $0.textColor = UIColor(named: "normalText")
+        $0.font = .pretendard(size: 24, weight: .bold)
+        $0.asColor(targetString: "권한", color: UIColor(named: "mainOrange"))
+    }
+    
+    lazy var guideDetailLabel = UILabel().then {
+        $0.text = "원활한 서비스 이용을 위해 동의가 필요해요."
+        $0.textColor = UIColor(named: "textGray")
+        $0.font = .pretendard(size: 16, weight: .medium)
+    }
+    
+    lazy var microphoneImageView = UIImageView().then {
+        $0.image = UIImage(named: "microphone")
+        $0.contentMode = .scaleAspectFill
+    }
+    
+    lazy var recordingLabel = UILabel().then {
+        $0.text = "주인장 오디오 녹음 (필수)"
+        $0.textColor = UIColor(named: "textBlack")
+        $0.font = .pretendard(size: 16, weight: .semiBold)
+        $0.asColor(targetString: "(필수)", color: UIColor(named: "mainOrange"))
+    }
+    
+    lazy var recordingDetailLabel = UILabel().then {
+        $0.text = "녹음 및 음성인식 서비스 제공"
+        $0.textColor = UIColor(named: "textBlack")
+        $0.font = .pretendard(size: 14, weight: .medium)
+    }
+    
+    lazy var confirmButton = UIButton().then {
+        $0.setTitle("확인", for: .normal)
+        $0.setTitleColor(UIColor(named: "textWhite"), for: .normal)
+        $0.backgroundColor = UIColor(named: "textBlack")
+        $0.layer.cornerRadius = 8
+        $0.addTarget(self, action: #selector(buttonTapped(_:)), for: .touchUpInside)
+        
+        $0.titleLabel?.font = .pretendard(size: 16, weight: .semiBold)
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .white
+        setNavigationBar()
+        addSubViews()
+        setupLayout()
+    }
+    
+    func setNavigationBar() {
+        self.navigationController?.navigationBar.tintColor = .black
+        self.navigationItem.hidesBackButton = true
+        let backButtonImage = UIImage(named: "arrow-left")
+        let backButton = UIBarButtonItem(image: backButtonImage, style: .plain,target: self, action: #selector(backButtonTapped))
+        navigationItem.leftBarButtonItem = backButton
+    }
+    
+    func addSubViews() {
+        [guideLabel,
+         guideDetailLabel,
+         microphoneImageView,
+         recordingLabel,
+         recordingDetailLabel,
+         confirmButton].forEach { view.addSubview($0) }
+    }
+    
+    func setupLayout() {
+        // 안내 Label
+        guideLabel.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaLayoutGuide.snp.top).offset(0.04 * view.bounds.height)
+            $0.leading.equalTo(24)
+        }
+
+        // 안내 상세 Label
+        guideDetailLabel.snp.makeConstraints {
+            $0.top.equalTo(guideLabel.snp.bottom).offset(0.06 * view.bounds.height)
+            $0.height.lessThanOrEqualTo(0.08 * view.bounds.height)
+            $0.leading.equalTo(24)
+        }
+        
+        // 마이크 ImageView
+        microphoneImageView.snp.makeConstraints {
+            $0.top.equalTo(guideDetailLabel.snp.bottom).offset(59)
+            $0.leading.equalToSuperview().offset(37)
+        }
+        
+        // 녹음 Label
+        recordingLabel.snp.makeConstraints {
+            $0.top.equalTo(guideDetailLabel.snp.bottom).offset(58)
+            $0.leading.equalTo(microphoneImageView.snp.trailing).offset(10)
+        }
+        
+        // 녹음 상세 Label
+        recordingDetailLabel.snp.makeConstraints {
+            $0.top.equalTo(recordingLabel.snp.bottom).offset(8)
+            $0.leading.equalTo(69)
+        }
+
+        // 확인 Button
+        confirmButton.snp.makeConstraints {
+            $0.centerX.equalToSuperview()
+            $0.bottom.equalTo(view.safeAreaLayoutGuide.snp.bottom).offset(-33)
+            $0.leading.equalToSuperview().offset(24)
+            $0.trailing.equalToSuperview().offset(-24)
+            $0.height.equalTo(52)
+        }
+    }
+    
+    @objc func backButtonTapped() {
+        let SignupPopupVC = SignupPopupViewController()
+        SignupPopupVC.modalPresentationStyle = .overCurrentContext
+        present(SignupPopupVC, animated: false, completion: nil)
+    }
+    
+    @objc func buttonTapped(_ sender: UIButton) {
+        // -TODO: 녹음 권한 팝업창 띄우기
+        let mainVC = MainViewController()
+        mainVC.modalPresentationStyle = .fullScreen
+        navigationItem.backBarButtonItem = UIBarButtonItem(title: "", style: .plain, target: nil, action: nil)
+        self.navigationController?.pushViewController(mainVC, animated: true)
+    }
+}
