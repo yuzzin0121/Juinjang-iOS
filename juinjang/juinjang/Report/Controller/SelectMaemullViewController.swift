e1e355293930e3c06415186e849211f33caa2022
do-dop <pdy0207@naver.com>
2024-01-31 15:05:46 +0900

    [Feat]비교할 매물 선택

--- juinjang/juinjang/Report/Controller/SelectMaemullViewController.swift
+++ juinjang/juinjang/Report/Controller/SelectMaemullViewController.swift
@@ -33,9 +33,35 @@ class SelectMaemullViewController : UIViewController {
         $0.backgroundColor = .white
     }
     
-    var tableView = UITableView().then {
-        $0.backgroundColor = .purple
-    }
+    lazy var filterselectBtn: UIButton = {
+        var configuration = UIButton.Configuration.filled()
+        var container = AttributeContainer()
+        container.font = .pretendard(size:14, weight: .semiBold)
+        configuration.attributedTitle = AttributedString(filterList[0].title, attributes: container)
+        configuration.imagePadding = 2
+        let button = UIButton(configuration: configuration, primaryAction: nil)
+        button.setTitleColor(ColorStyle.darkGray, for: .normal)
+        button.setImage(ImageStyle.arrowDown, for: .normal)
+        button.semanticContentAttribute = .forceRightToLeft
+        button.contentHorizontalAlignment = .leading
+        button.tintColor = .white
+        return button
+    }()
+    
+    var menuChildren: [UIMenuElement] = []
+    lazy var filterList = Filter.allCases
+    var imjangList: [ImjangNote] = ImjangList.list
+    
+    let tableView: UITableView = {
+        let tableView = UITableView(frame: .zero)
+        tableView.estimatedRowHeight = UITableView.automaticDimension
+        tableView.separatorStyle = .none
+        tableView.showsVerticalScrollIndicator = false
+        tableView.backgroundColor = .white
+        tableView.contentInset = UIEdgeInsets.init(top: 5, left: 0, bottom: 0, right: 0)
+        tableView.register(ReportImjangListTableViewCell.self, forCellReuseIdentifier: ReportImjangListTableViewCell.identifier)
+        return tableView
+    }()
     
     var btnBackGroundView = UIView().then{
         $0.backgroundColor = .white
@@ -47,6 +73,33 @@ class SelectMaemullViewController : UIViewController {
         $0.setTitle("적용하기", for: .normal)
         $0.titleLabel?.font = UIFont(name: "Pretendard-SemiBold", size: 16)
         $0.setTitleColor(UIColor.white, for: .normal)
+        
+    }
+    @objc
+    func applyBtnTap(_ sender: UIButton) {
+        let vc = ReportViewController()
+        vc.modalPresentationStyle = .overFullScreen
+       // vc.tabViewController.defaultPage(for: CompareViewController())
+        self.present(vc, animated: false)
+    }
+    
+    func setFilterData() {
+        for filter in filterList {
+            menuChildren.append(UIAction(title: filter.title, state: .off,handler: {  (action: UIAction) in
+                self.changefilterTitle(filter.title)
+                self.callRequestFiltered(sort: filter.title)
+            }))
+        }
+        filterselectBtn.menu = UIMenu(options: .displayAsPalette, children: menuChildren)
+        
+        filterselectBtn.showsMenuAsPrimaryAction = true
+    }
+    func changefilterTitle(_ title: String) {
+        filterselectBtn.setTitle(title, for: .normal)
+        filterselectBtn.titleLabel?.font = UIFont(name: "Pretendard-SemiBold", size: 14)
+    }
+    func callRequestFiltered(sort: String) {
+        
     }
     
     func setConstraint() {
@@ -79,6 +132,12 @@ class SelectMaemullViewController : UIViewController {
             $0.left.right.equalToSuperview()
             $0.height.equalTo(63)
         }
+        filterselectBtn.snp.makeConstraints{
+            $0.top.equalToSuperview().offset(32)
+            $0.left.equalToSuperview().offset(20)
+            $0.height.equalTo(19)
+            //$0.width.equalTo(73.3)
+        }
         tableView.snp.makeConstraints{
             $0.top.equalTo(contentView.snp.bottom)
             $0.left.right.equalToSuperview()
@@ -89,17 +148,54 @@ class SelectMaemullViewController : UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
         view.backgroundColor = .white
         
         view.addSubview(backButton)
         view.addSubview(compareLabel)
         view.addSubview(searchButton)
         view.addSubview(contentView)
+        contentView.addSubview(filterselectBtn)
         view.addSubview(tableView)
         
         view.addSubview(btnBackGroundView)
         btnBackGroundView.addSubview(applyBtn)
+        applyBtn.addTarget(self, action: #selector(applyBtnTap), for: .touchUpInside)
         
+        setFilterData()
         setConstraint()
     }
 }
+
+extension SelectMaemullViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return imjangList.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: ReportImjangListTableViewCell.identifier, for: indexPath) as! ReportImjangListTableViewCell
+        cell.selectionStyle = .none
+        cell.configureCell(imjangNote: imjangList[indexPath.row])
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        var cell = tableView.cellForRow(at: indexPath as IndexPath)!
+        cell.contentView.backgroundColor = UIColor(named: "main100")
+        cell.contentView.layer.borderColor = UIColor(named: "juinjang")?.cgColor
+        applyBtn.backgroundColor = UIColor(named: "500")
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 116
+    }
+
+}
+
+
+#Preview {
+    SelectMaemullViewController()
+}
+
