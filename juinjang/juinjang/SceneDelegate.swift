28a49a08c2a311b390062c2aa7b89187c3fb4f42
yuzzin <ydbwlsjki60@gmail.com>
2024-08-21 11:21:44 +0900

    [Feat] OnboardingContainerViewController 구현

--- juinjang/juinjang/SceneDelegate.swift
+++ juinjang/juinjang/SceneDelegate.swift
@@ -20,7 +20,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
 
-        let mainViewController = SplashViewController()
+        let mainViewController = OnboardingContainerViewController()
         window?.rootViewController = mainViewController
         window?.makeKeyAndVisible()
         
---
+++ juinjang/juinjang/UserInterface/Onboarding/OnboardingContainerViewController.swift
@@ -0,0 +1,92 @@
+//
+//  OnboardingContainerViewController.swift
+//  juinjang
+//
+//  Created by 조유진 on 8/21/24.
+//
+
+import UIKit
+
+final class OnboardingContainerViewController: UIViewController {
+    private let goLoginButton = UIButton()
+    private let pageViewController = UIPageViewController(transitionStyle: .scroll, navigationOrientation: .horizontal)
+    private var pageViewControllerList = [UIViewController]()
+    private var initialPage = 0
+    
+    private var pageControl: UIPageControl!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureDataSource()
+        configureHierarchy()
+        configureLayout()
+        configureView()
+    }
+    
+    private func configureDataSource() {
+        pageViewController.dataSource = self
+    }
+    
+    private func configureHierarchy() {
+        view.addSubview(pageViewController.view)
+        addChild(pageViewController)
+        
+        view.addSubview(pageControl)
+    }
+    
+    private func configureLayout() {
+        pageViewController.view.snp.makeConstraints { make in
+            make.edges.equalTo(view.safeAreaLayoutGuide)
+        }
+        
+        pageControl.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide).offset(18)
+            make.leading.equalTo(view.safeAreaLayoutGuide).offset(24)
+        }
+    }
+    
+    private func configureView() {
+        pageControl = UIPageControl.appearance()
+        pageControl.currentPageIndicatorTintColor = ColorStyle.mainOrange
+        pageControl.pageIndicatorTintColor = ColorStyle.lightBackgroundOrange
+        pageControl.backgroundColor = .clear
+        
+        pageControl.numberOfPages = pageViewControllerList.count
+        
+        guard let first = pageViewControllerList.first else { return }
+        pageViewController.setViewControllers([first], direction: .forward, animated: true)
+    }
+}
+
+extension OnboardingContainerViewController: UIPageViewControllerDataSource {
+    // 이전 화면에 대한 구성
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+        // 1. 현재 페이지뷰컨에 보이는 뷰컨의 인덱스를 가지고 오기
+        // 2. 그 인덱스의 -1 값인 뷰컨을 리턴
+        guard let currentIndex = pageViewControllerList.firstIndex(of: viewController) else { return nil }
+        let previousIndex = currentIndex - 1
+        
+        return previousIndex < 0 ? nil : pageViewControllerList[previousIndex]
+    }
+    
+    // 다음 화면에 대한 구성
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+        // 1. 현재 페이지뷰컨에 보이는 뷰컨의 인덱스를 가지고 오기
+        // 2. 그 인덱스의 +1 값인 뷰컨을 리턴
+        guard let currentIndex = pageViewControllerList.firstIndex(of: viewController) else { return nil }
+        let nextIndex = currentIndex + 1
+        
+        return nextIndex >= pageViewControllerList.count ? nil : pageViewControllerList[nextIndex]
+    }
+    
+    func presentationCount(for pageViewController: UIPageViewController) -> Int {
+        return pageViewControllerList.count
+    }
+    
+    func presentationIndex(for pageViewController: UIPageViewController) -> Int {
+        guard let viewController = pageViewController.viewControllers?.first,
+              let currentIndex = pageViewControllerList.firstIndex(of: viewController) else { return 0 }
+        return currentIndex
+    }
+}
